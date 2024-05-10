e5fe9eab4ca74c9421c660a9e98b64aef37b0e83
pradosh-arduino <pradoshnathan@gmail.com>
2024-05-10 15:42:13 +0530

    Added logging.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 ﻿using System;
 
+class Logging{
+    public static void info(string message){
+        Console.WriteLine("\x1b[94m[Info] " + message + "\x1b[0m");
+    }
+    public static void warn(string message){
+        Console.WriteLine("\x1b[93m[Warn] " + message + "\x1b[0m");
+    }
+    public static void error(string message){
+        Console.WriteLine("\x1b[91m[Error] " + message + "\x1b[0m");
+    }
+}
+
 class Nebula{
     public static void Main(string[] args){
-        Console.WriteLine("Hello, World!");
+        Nebula self = new Nebula();
+        
+        Logging.warn("Test warn!");
     }
 }
