255cba97c67089902c3cb1015448445e10e8ef76
pradosh-arduino <pradoshnathan@gmail.com>
2024-05-10 15:27:45 +0530

    Added classes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Nebula{
+    public static void Main(string[] args){
+        Console.WriteLine("Hello, World!");
+    }
+}
